a5a5e08cf12d954beb2d454e606ae1119aa01915
darved2305 <darshanmved@gmail.com>
2025-11-22 23:42:16 +0530

    logo added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -9,7 +9,16 @@ const NavBar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 md:p-6 p-3">
-      <div className="flex justify-end items-center">
+      <div className="flex justify-between items-center">
+        {/* Logo - Top Left */}
+        <div className="flex items-center">
+          <img 
+            src="/images/logomediguard.png" 
+            alt="MediGuard AI" 
+            className="h-20 md:h-24 w-auto"
+          />
+        </div>
+        
         <button className="nav-cta" onClick={handleGetStarted}>
           Get Started
         </button>
